7bc6973df916b7493f250f0debb9b21de5563b0a
labibahmed10 <labib.ahmed.372@gmail.com>
2022-04-30 11:22:34 +0600

    fixed firebase related issue

--- src/Components/Spinner/Spinner.js
+++ src/Components/Spinner/Spinner.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div>
+    <div className="flex items-center justify-center h-[90vh]">
       <svg
         role="status"
-        className="inline w-12 h-12 mr-2 text-gray-200 animate-spin dark:text-gray-300 fill-[#9B5A43]"
+        className="inline w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-gray-300 fill-[#9B5A43]"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,17 @@
+import { getAuth } from "firebase/auth";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBbv-F23s1pjPq-p9rvAgTU4W01p_Udhn0",
-  authDomain: "cologne-perfume-auth.firebaseapp.com",
-  projectId: "cologne-perfume-auth",
-  storageBucket: "cologne-perfume-auth.appspot.com",
-  messagingSenderId: "192614512557",
-  appId: "1:192614512557:web:800ee78418f0bb030f1324",
+  apiKey: "AIzaSyBYbCE-YqxthF2T_E95ObRLW2xqZR8pgVM",
+  authDomain: "cologne-perfume-auth-6e250.firebaseapp.com",
+  projectId: "cologne-perfume-auth-6e250",
+  storageBucket: "cologne-perfume-auth-6e250.appspot.com",
+  messagingSenderId: "180610178635",
+  appId: "1:180610178635:web:024dea9c9e8cca8a66f7ce",
 };
 
 // Initialize Firebase
