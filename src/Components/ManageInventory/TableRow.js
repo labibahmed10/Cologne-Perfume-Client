e2401705fed2fa654bb8d10aef3847993f6bae67
labibahmed10 <labib.ahmed.372@gmail.com>
2022-05-03 23:59:36 +0600

    pagination implemented at manage inventory page

--- src/Components/ManageInventory/TableRow.js
+++ src/Components/ManageInventory/TableRow.js
@@ -8,7 +8,7 @@ const TableRow = ({ item, i, handleDeleteProduct }) => {
   const navigate = useNavigate();
 
   return (
-    <tr className="bg-white border-b">
+    <tr className="bg-white  border-b">
       <td className="px-6 py-4 whitespace-nowrap text-xl font-medium text-gray-600">{i + 1}</td>
       <td className="text-xl text-gray-600 font-light px-6 py-4 whitespace-nowrap">{name}</td>
       <td>
